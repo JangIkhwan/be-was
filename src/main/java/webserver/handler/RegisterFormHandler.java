b8a818427538d5203b0fcbd9e4664cce3253fdad
JangIkhwan <bluensky0213@gmail.com>
2026-01-02 10:39:31 +0900

    refactor : 회원가입 폼 요청은 리다이렉트로 변경

--- src/main/java/webserver/handler/RegisterFormHandler.java
+++ src/main/java/webserver/handler/RegisterFormHandler.java
@@ -3,23 +3,11 @@ package webserver.handler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 public class RegisterFormHandler implements Handler{
     private static final Logger logger = LoggerFactory.getLogger(RegisterFormHandler.class);
 
     @Override
     public Response handle(Request request) {
-        try {
-            byte[]  body = Files.readAllBytes(Path.of("./src/main/resources/static/registration/index.html"));
-            return Response.ok(body, "text/html");
-        }
-        catch (IOException e) {
-            logger.error("error occurred while reading static resource");
-        }
-        return Response.notFound();
-
+        return Response.redirect("/registration/index.html");
     }
 }
