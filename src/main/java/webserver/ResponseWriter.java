156dbbaa3cc2181db5f50d2c6582385ae0ed1af8
JangIkhwan <bluensky0213@gmail.com>
2026-01-02 11:01:36 +0900

    refactor : http 응답 쓰기 로직을 추상화
    - 응답을 쓰는 작업을 status line, header, body를 작성 3가지로 보고 각각을 메서드로 분리

--- src/main/java/webserver/ResponseWriter.java
+++ src/main/java/webserver/ResponseWriter.java
@@ -17,63 +17,44 @@ public class ResponseWriter {
          dos = new DataOutputStream(out);
     }
 
-    public void write200Response(byte[] body, String contentType){
-        response200Header(contentType, body.length);
-        responseBody(body);
-    }
-
-    private void response200Header(String contentType, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: " + contentType + ";charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    public void write404Response() {
-        try {
-            dos.writeBytes("HTTP/1.1 404 NOT FOUND \r\n");
-            dos.writeBytes("\r\n");
-            dos.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    public void write303Response(String redirectUrl) {
-        try {
-            dos.writeBytes("HTTP/1.1 303 See Other\r\n");
-            dos.writeBytes("Location: " + redirectUrl + "\r\n");
-            dos.writeBytes("Content-Length: 0\r\n");
-            dos.writeBytes("Connection: close\r\n");
-            dos.writeBytes("\r\n");
-            dos.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
     public void write(Response response) {
-        if(response.isNotFound()){
-            write404Response();
-            return;
+        writeStatusLine(response);
+        writeHeader(response);
+        writeBody(response);
+    }
+
+    private void writeStatusLine(Response response) {
+        try{
+            dos.writeBytes("HTTP/1.1 " + response.getCode() + " " + response.getCodeDescription() + "\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
         }
-        if(response.isRedirect()){
-            write303Response(response.getRedirectUrl());
-            return;
+    }
+
+    private void writeHeader(Response response) {
+        try {
+            if(response.isRedirect()){
+                dos.writeBytes("Location: " + response.getRedirectUrl() + "\r\n");
+            }
+            if(response.hasBody()){
+                dos.writeBytes("Content-Type: " + response.getContentType() + "\r\n");
+                dos.writeBytes("Content-Length: " + response.getBody().length + "\r\n");
+            }
+            dos.writeBytes("\r\n");
+            dos.flush();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
         }
-        write200Response(response.getBody(), response.getContentType());
+    }
+
+    private void writeBody(Response response) {
+        try {
+            if(response.hasBody()){
+                dos.write(response.getBody(), 0, response.getBody().length);
+            }
+            dos.flush();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
     }
 }
--- src/main/java/webserver/handler/Response.java
+++ src/main/java/webserver/handler/Response.java
@@ -5,8 +5,13 @@ public class Response {
     private byte[] body;
     private String contentType;
     private String redirectUrl;
+    private String codeDescription;
 
     public Response(){ }
+
+    public int getCode() {
+        return code;
+    }
 
     public byte[] getBody() {
         return body;
@@ -17,15 +22,19 @@ public class Response {
     }
 
     public String getRedirectUrl() {
-        return this.redirectUrl;
+        return redirectUrl;
+    }
+
+    public String getCodeDescription() {
+        return codeDescription;
     }
 
     public boolean isRedirect() {
         return code == 303;
     }
 
-    public boolean isNotFound() {
-        return code == 404;
+    public boolean hasBody(){
+        return body != null;
     }
 
     public static Response ok(byte[] body, String contentType){
@@ -33,6 +42,7 @@ public class Response {
         response.code = 200;
         response.body = body;
         response.contentType = contentType;
+        response.codeDescription = "OK";
         return response;
     }
 
@@ -40,12 +50,14 @@ public class Response {
         Response response = new Response();
         response.code = 303;
         response.redirectUrl = redirectUrl;
+        response.codeDescription = "SEE OTHERS";
         return response;
     }
 
     public static Response notFound(){
         Response response = new Response();
         response.code = 404;
+        response.codeDescription = "NOT FOUND";
         return response;
     }
 }
