eb869ab7b28a751a7684f2896e55a37a6d201cf1
JangIkhwan <bluensky0213@gmail.com>
2026-01-05 15:55:11 +0900

    feat : 4단계 - http 요청 파싱 에러나 서버 내부 에러 시 예외 처리 추가
    - http 요청 파싱 관련 예외 선언
    - 예외 발생 시 상태 코드 400을 응답하는 예외처리 구현
    - 서버 내부 오류 발생 시 상태 코드 500을 응답하는 예외처리 구현
    - 상태 코드 400, 500 관련 enum 상수 추가
    - Response에 정적 팩토리 메서드 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import webserver.exception.RequestParsingException;
 import webserver.handler.*;
 
 public class RequestHandler implements Runnable {
@@ -32,16 +33,23 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            RequestGenerator requestGenerator = new RequestGenerator(in);
-            Request request = requestGenerator.generate();
-
-            logger.debug("request parsing complete");
-
-            Handler handler = resovleHandler(request.getHandlerKey());
-            Response response = handler.handle(request);
-
             ResponseWriter responseWriter = new ResponseWriter(out);
-            responseWriter.write(response);
+            try{
+                RequestGenerator requestGenerator = new RequestGenerator(in);
+                Request request = requestGenerator.generate();
+
+                logger.debug("request parsing complete");
+
+                Handler handler = resovleHandler(request.getHandlerKey());
+                Response response = handler.handle(request);
+                responseWriter.write(response);
+            }
+            catch (RequestParsingException e){
+                responseWriter.write(Response.badRequest());
+            }
+            catch (RuntimeException e){
+                responseWriter.write(Response.internalServerError());
+            }
 
         } catch (IOException e) {
             logger.error(e.getMessage());
