02d422b51e0bc2e4839367fc2ed6057c0ebea028
JangIkhwan <bluensky0213@gmail.com>
2026-01-07 17:08:28 +0900

    test : 6단계 - MyPageHandler 테스트 작성

---
+++ src/test/java/webserver/handler/MyPageHandlerTest.java
@@ -0,0 +1,56 @@
+package webserver.handler;
+
+import model.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import webserver.service.AuthService;
+import webserver.session.SessionStore;
+
+import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static webserver.constant.ResponseStatusCode.OK;
+import static webserver.constant.ResponseStatusCode.SEE_OTHER;
+
+class MyPageHandlerTest {
+    @DisplayName("로그인하지 않은 사용자는 리다이렉트한다")
+    @Test
+    void shouldRedirect_whenUserNotLoggedIn(){
+        // given
+        AuthService authService = new AuthService();
+        MyPageHandler myPageHandler = new MyPageHandler(authService);
+
+        SessionStore sessionStore = new SessionStore();
+
+        Request request = new Request("GET", "/mypage", new HashMap<>(), new HashMap<>(), sessionStore);
+
+        // when
+        Response response = myPageHandler.handle(request);
+
+        // then
+        assertThat(response.getCode()).isEqualTo(SEE_OTHER.getCode());
+    }
+
+    @DisplayName("로그인한 사용자는 마이페이지를 반환한다")
+    @Test
+    void shouldReturnMyPage_whenUserLoggedIn(){
+        // given
+        AuthService authService = new AuthService();
+        MyPageHandler myPageHandler = new MyPageHandler(authService);
+
+        HashMap<String, String> header = new HashMap<>();
+        header.put("Cookie", "sid=1234");
+
+        SessionStore sessionStore = new SessionStore();
+        sessionStore.addSession("1234", new User("asdf", "asdf", "asdf", "asdf"));
+
+        Request request = new Request("GET", "/mypage", header, new HashMap<>(), sessionStore);
+
+        // when
+        Response response = myPageHandler.handle(request);
+
+        // then
+        assertThat(response.getCode()).isEqualTo(OK.getCode());
+    }
+}
