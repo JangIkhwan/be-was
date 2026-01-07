88c918e81d6e89c81fcbfd8c48e51e300f512f68
JangIkhwan <bluensky0213@gmail.com>
2026-01-07 16:30:00 +0900

    test : 5단계 - LogoutHandler 테스트 작성

---
+++ src/test/java/webserver/handler/LogoutHandlerTest.java
@@ -0,0 +1,35 @@
+package webserver.handler;
+
+import model.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import webserver.session.SessionStore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static webserver.constant.ResponseStatusCode.SEE_OTHER;
+
+class LogoutHandlerTest {
+    @DisplayName("올바른 요청이면 리다이렉트한다")
+    @Test
+    void shouldRedirect_whenCorrectRequest(){
+        // given
+        LogoutHandler logoutHandler = new LogoutHandler();
+        Map<String, String> header = new HashMap<>();
+        header.put("Cookie", "sid=1234");
+
+        SessionStore sessionStore = new SessionStore();
+        sessionStore.addSession("1234", new User("asdf", "asdf", "asdf", "asdf"));
+
+        Request request = new Request("POST", "/logout", header, new HashMap<>(), sessionStore);
+
+        // when
+        Response response = logoutHandler.handle(request);
+
+        // then
+        assertThat(response.getCode()).isEqualTo(SEE_OTHER.getCode());
+        assertThat(response.getHeader("Location")).isEqualTo("/");
+    }
+}
