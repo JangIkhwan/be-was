2f7848dc9184cbf83257d4f98a38cfc96504e9d5
JangIkhwan <bluensky0213@gmail.com>
2026-01-07 14:52:49 +0900

    feat : 6단계 - MainHandler에서 인증된 사용자인 경우에는 동적 HTML을 생성하도록 수정
    - 로그인한 사용자의 이름을 표시할 수 있는 HTML을 추가
    - MainHandler에 인증 여부 검증 추가

--- src/main/java/webserver/handler/MainHandler.java
+++ src/main/java/webserver/handler/MainHandler.java
@@ -1,7 +1,40 @@
 package webserver.handler;
 
+import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.session.SessionStore;
+
+import java.util.Map;
+
 public class MainHandler implements Handler {
+    private static final Logger logger = LoggerFactory.getLogger(MainHandler.class);
+
     public Response handle(Request request) {
-        return Response.forward("/index.html");
+        logger.debug("request = {}", request);
+
+        String sid = request.getCookie("sid");
+        if(!foundCookie(sid)) {
+            return Response.forward("/index.html");
+        }
+
+        SessionStore sessionStore = request.getSessionStore();
+        User loginedUser = (User) sessionStore.getSession(sid);
+        if(!hasLoggedIn(loginedUser)){
+            return Response.forward("/index.html");
+        }
+
+        logger.debug("session found");
+
+        Map<String, String> model = Map.of("name", loginedUser.getName());
+        return Response.forwardDynamicHtml(model, "/index_logined.html");
+    }
+
+    private boolean foundCookie(String sid) {
+        return sid != null;
+    }
+
+    private boolean hasLoggedIn(User loginedUser) {
+        return loginedUser != null;
     }
 }
