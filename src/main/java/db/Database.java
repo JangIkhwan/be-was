76ac828122831fc6bc853996dd59fe96e4830e6b
JangIkhwan <bluensky0213@gmail.com>
2026-01-07 17:30:37 +0900

    WIP

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -5,10 +5,13 @@ import model.User;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 public class Database {
     private static Map<String, User> users = new HashMap<>();
+
+    static {
+        addUser(new User("asdf", "asdf", "asdf", "asdf"));
+    }
 
     public static void addUser(User user) {
         users.put(user.getUserId(), user);
