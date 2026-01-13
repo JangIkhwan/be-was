9c78e97c3ac2bec61b884689be5f690dbf091700
JangIkhwan <bluensky0213@gmail.com>
2026-01-13 11:38:18 +0900

    fix : 7단계 - User의 식별자를 DB의 PK 변경
    - userId 필드를 삭제하고 id 필드를 추가

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -3,26 +3,25 @@ package model;
 import webserver.exception.BusinessException;
 
 public class User {
-    private String userId;
+    private Long id;
     private String password;
     private String name;
     private String email;
 
-    public User(String userId, String password, String name, String email) {
-        if(userId.isBlank() || password.isBlank() || name.isBlank() || email.isBlank()){
+    public User(String password, String name, String email) {
+        if (password.isBlank() || name.isBlank() || email.isBlank()) {
             throw new BusinessException();
         }
-        if(userId == null || password == null|| name == null || email == null){
+        if (password == null || name == null || email == null) {
             throw new BusinessException();
         }
-        this.userId = userId;
         this.password = password;
         this.name = name;
         this.email = email;
     }
 
-    public String getUserId() {
-        return userId;
+    public Long getId() {
+        return id;
     }
 
     public String getPassword() {
@@ -37,8 +36,12 @@ public class User {
         return email;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @Override
     public String toString() {
-        return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
+        return "User [userId=" + id + ", password=" + password + ", name=" + name + ", email=" + email + "]";
     }
 }
