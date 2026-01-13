f4764412fe2774bdf839a03800c07052e8ff6f03
JangIkhwan <bluensky0213@gmail.com>
2026-01-13 17:30:46 +0900

    feat : 7단계 - index_logined.html에서 템플릿을 적용할 수 있도록 기능 MainPageDynamicView 작성

---
+++ src/main/java/webserver/mvc/MainPageDynamicView.java
@@ -0,0 +1,75 @@
+package webserver.mvc;
+
+import model.Article;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.constant.FileMimeType;
+import webserver.exception.StaticResourceNotFoundException;
+import webserver.http.Response;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+public class MainPageDynamicView implements ModelAndView{
+    private static final Logger logger = LoggerFactory.getLogger(MainPageDynamicView.class);
+    private Map<String, Object> model;
+    private String viewName;
+
+    public MainPageDynamicView(Map<String, Object> model, String viewName) {
+        this.model = model;
+        this.viewName = viewName;
+    }
+
+    @Override
+    public String getViewName() {
+        return viewName;
+    }
+
+    @Override
+    public void render(Response response) {
+        try{
+            Path filePath = Paths.get("./src/main/resources/static" + getViewName());
+            String baseHtml = Files.readString(filePath, StandardCharsets.UTF_8);
+
+            logger.debug("before baseHtml = {} " , baseHtml);
+
+            if(model.containsKey("name")){
+                baseHtml = baseHtml.replace("${{name}}", (String) model.get("name"));
+            }
+
+            if(model.containsKey("article")){
+                Article article = (Article) model.get("article");
+                Path templatePath = Paths.get("./src/main/resources/static" + "/template/article.html");
+                String articleTemplate = Files.readString(templatePath, StandardCharsets.UTF_8);
+                logger.debug("articleTemplate = {}", articleTemplate);
+
+                articleTemplate = articleTemplate.replace("${{writer}}", article.getWriterName());
+                articleTemplate = articleTemplate.replace("${{content}}", article.getContent());
+
+                baseHtml = baseHtml.replace("${{article}}", articleTemplate);
+            }
+            else{
+                baseHtml = baseHtml.replace("${{article}}", "첫 게시글을 써주세요");
+            }
+
+            if(!model.containsKey("comments")){
+                baseHtml = baseHtml.replace("${{comments}}", "댓글이 없습니다");
+                baseHtml = baseHtml.replace("${{all-comments-button}}", "");
+            }
+
+            logger.debug("after baseHtml = {} " , baseHtml);
+
+            byte[] body = baseHtml.getBytes(StandardCharsets.UTF_8);
+            String contentType = FileMimeType.resolveMimeType(getViewName());
+            response.setOk(body, contentType);
+        }
+        catch (IOException e) {
+            logger.error("error occurred while reading static resource");
+            throw new StaticResourceNotFoundException();
+        }
+    }
+}
