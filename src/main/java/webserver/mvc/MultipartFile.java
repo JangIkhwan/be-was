2fc4f6bb22a23561f0586f444399ed75e92f0714
JangIkhwan <bluensky0213@gmail.com>
2026-01-14 19:07:14 +0900

    feat : 7단계 - MultipartFile 구현

--- src/main/java/webserver/mvc/MultipartFile.java
+++ src/main/java/webserver/mvc/MultipartFile.java
@@ -3,6 +3,19 @@ package webserver.mvc;
 import java.io.InputStream;
 
 public class MultipartFile {
+    private String filename;
+    private InputStream inputStream;
+
     public MultipartFile(String filename, InputStream inputStream) {
+        this.filename = filename;
+        this.inputStream = inputStream;
+    }
+
+    public String getFilename(){
+        return filename;
+    }
+
+    public InputStream getInputStream() {
+        return inputStream;
     }
 }
