b005dfa20b5b1a7adb355b2032c1cb512898f35d
JangIkhwan <bluensky0213@gmail.com>
2026-01-15 11:11:15 +0900

    feat : 7단계 - CreateArticleHandler에서 이미지를 저장하는 로직 추가
    - 이미지가 없으면 예외 발생
    - 이미지를 로컬에 저장

--- src/main/java/webserver/handler/CreateArticleHandler.java
+++ src/main/java/webserver/handler/CreateArticleHandler.java
@@ -3,12 +3,23 @@ package webserver.handler;
 import db.ArticleRepository;
 import model.Article;
 import model.User;
+import webserver.exception.BadRequestException;
 import webserver.http.Request;
 import webserver.http.Response;
 import webserver.mvc.Handler;
 import webserver.mvc.ModelAndView;
+import webserver.mvc.MultipartFile;
 import webserver.mvc.RedirectView;
 import webserver.util.AuthUtil;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.UUID;
 
 public class CreateArticleHandler implements Handler {
     private final ArticleRepository articleRepository;
@@ -26,8 +37,43 @@ public class CreateArticleHandler implements Handler {
         String title = request.getParameter("title");
         String content = request.getParameter("content");
 
-        articleRepository.save(new Article(user.getId(), title, content));
+        if(request.getMultipartFiles().size() != 1){
+            // todo 게시글 작성폼에서 에러문구 출력하도록 수정
+            throw new BadRequestException();
+        }
+
+        MultipartFile multipartFile = request.getMultipartFiles().get(0);
+
+        String imageUrl = null;
+        try {
+            imageUrl = saveFile(multipartFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        articleRepository.save(new Article(user.getId(), title, content, imageUrl));
 
         return new RedirectView("/");
+    }
+
+    private String saveFile(MultipartFile multipartFile) throws IOException {
+        InputStream in = multipartFile.getInputStream();
+        String filename = multipartFile.getFilename() + UUID.randomUUID().toString();
+        String projectRoot = System.getProperty("user.dir");
+        Path uploadDir = Paths.get(projectRoot, "uploads", "images");
+
+        Files.createDirectories(uploadDir);
+
+        Path target = uploadDir.resolve(filename);
+
+        try (OutputStream out = Files.newOutputStream(
+                target,
+                StandardOpenOption.CREATE,
+                StandardOpenOption.TRUNCATE_EXISTING)) {
+
+            in.transferTo(out);
+        }
+
+        return filename;
     }
 }
