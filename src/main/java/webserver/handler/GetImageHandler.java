2c564ea8921191499e2b66e7a6ca5f03e3f4c597
JangIkhwan <bluensky0213@gmail.com>
2026-01-15 15:10:49 +0900

    feat : 7단계 - 업로드된 이미지를 전송하는 GetImageHandler 작성

---
+++ src/main/java/webserver/handler/GetImageHandler.java
@@ -0,0 +1,19 @@
+package webserver.handler;
+
+import webserver.exception.StaticResourceNotFoundException;
+import webserver.http.Request;
+import webserver.http.Response;
+import webserver.mvc.Handler;
+import webserver.mvc.UploadedFileView;
+import webserver.mvc.ModelAndView;
+
+public class GetImageHandler implements Handler {
+    @Override
+    public ModelAndView handle(Request request, Response response) {
+        String imageFileUrl = request.getParameter("imageUrl");
+        if(imageFileUrl == null){
+            throw new StaticResourceNotFoundException();
+        }
+        return new UploadedFileView("/uploads/images/" + imageFileUrl);
+    }
+}
