cd5c12e8543ae86187c92dab4615fb1152cd7584
JangIkhwan <bluensky0213@gmail.com>
2026-01-15 18:45:04 +0900

    feat : 7단계 - MyPageHandler에서 뷰에 전달하는 정보를 수정

--- src/main/java/webserver/handler/MyPageHandler.java
+++ src/main/java/webserver/handler/MyPageHandler.java
@@ -1,6 +1,8 @@
 package webserver.handler;
 
+import db.UserRepository;
 import model.User;
+import webserver.exception.BusinessException;
 import webserver.http.Request;
 import webserver.http.Response;
 import webserver.mvc.*;
@@ -12,16 +14,29 @@ import webserver.view.RedirectView;
 import java.util.HashMap;
 
 public class MyPageHandler implements Handler {
+    private final UserRepository userRepository;
+
+    public MyPageHandler(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public ModelAndView handle(Request request, Response response) {
         if(!AuthUtil.isAuthenticatedUser(request)){
             return new RedirectView("/login");
         }
 
-        User user = AuthUtil.getAuthenticatedUser(request);
+        User cachedUser = AuthUtil.getAuthenticatedUser(request);
+
+        User user = userRepository.findByEmail(cachedUser.getEmail())
+                .orElseThrow(() -> new BusinessException());
 
         HashMap<String, Object> model = new HashMap<>();
         model.put("name", user.getName());
+        String imageUrl = user.getImageUrl();
+        if(imageUrl != null){
+            model.put("profile_image", imageUrl);
+        }
         return new MyPageDynamicView(model, "/mypage/index.html");
     }
 }
