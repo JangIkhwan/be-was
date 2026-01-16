2bb37ec23398bb045db571b33ed8a02a4ea2ec32
JangIkhwan <bluensky0213@gmail.com>
2026-01-16 10:46:23 +0900

    chore : 7단계 - CreateArticleHandler에 로깅 추가

--- src/main/java/webserver/handler/CreateArticleHandler.java
+++ src/main/java/webserver/handler/CreateArticleHandler.java
@@ -3,6 +3,8 @@ package webserver.handler;
 import db.ArticleRepository;
 import model.Article;
 import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import webserver.http.Request;
 import webserver.http.Response;
 import webserver.mvc.*;
@@ -15,6 +17,7 @@ import webserver.view.StaticResourceView;
 import java.io.IOException;
 
 public class CreateArticleHandler implements Handler {
+    private static final Logger logger = LoggerFactory.getLogger(CreateArticleHandler.class);
     private final ArticleRepository articleRepository;
 
     public CreateArticleHandler(ArticleRepository articleRepository) {
@@ -31,6 +34,7 @@ public class CreateArticleHandler implements Handler {
         String content = request.getParameter("content");
 
         if(request.getMultipartFiles().size() != 1){
+            logger.debug("글 생성 요청에 이미지 없음");
             return new StaticResourceView("/article/error.html");
         }
 
